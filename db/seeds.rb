5e106e98d6600c3c284b36a0c066a099ce171fb8
Alex Vacca <alex.vacca@hotmail.com>
2017-11-07 13:51:59 +0100

    upload cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Restaurant.destroy_all
+Registration.destroy_all
+registration = Registration.create(
+  password:"11111111",
+  email: "thomas@hotmail.com"
+  )
+puts registration.user
+comme_chez_soi = Restaurant.create(
+  name: 'Comme chez Soi',
+  municipality: 'Center',
+  capacity: 50,
+  category: 'Belgian',
+  address: '23 place Rouppe',
+  user: registration.user
+)
+
+samourai = Restaurant.create(
+  name: 'Samourai',
+  municipality: 'Center',
+  capacity: 40,
+  category: 'Japanese',
+  address: 'Rue du Fossé aux Loups 28/B',
+  user: registration.user
+)
+
+dam_sum = Restaurant.create(
+  name: 'Dam Sum',
+  municipality: 'Ixelles',
+  capacity: 60,
+  category: 'Chinese',
+  address: 'Parvis de la Trinité 11',
+  user: registration.user
+)
+
+el_camion = Restaurant.create(
+  name: 'El Camion',
+  municipality: 'Center',capacity: (30),
+  category: 'Mexican',
+  address: 'Rue des Chapeliers 11',
+  user: registration.user
+)
+
+
+
+
+
+
+
