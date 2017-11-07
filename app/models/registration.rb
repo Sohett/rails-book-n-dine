a400a06d985a42b11d06a506d3617798c12c7e41
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-07 10:18:50 +0100

    style and routes guys

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,18 +3,18 @@ class Registration < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-  has_many :users
+  has_one :user
 
-  # before_action :create_user
-  #
-  #
-  # private
-  #
-  # def create_user
-  #   if registration_signed_in?
-  #     user = User.new
-  #     user.registration = registration_session
-  #   end
-  # end
+  after_create :create_user
 
+  private
+
+  # raise
+
+  def create_user
+    if registration_signed_in?
+      user = User.new
+      user.registration = registration_session
+    end
+  end
 end
